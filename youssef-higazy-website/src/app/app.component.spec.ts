49239788824c9a5fa377cc01b2188d8bb3deeda4
Youssef Higazy <yoyohigazy77@gmail.com>
2021-09-06 06:27:01 -0500

    Adding the first version

---
+++ youssef-higazy-website/src/app/app.component.spec.ts
@@ -0,0 +1,35 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'youssef-higazy-website'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('youssef-higazy-website');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('youssef-higazy-website app is running!');
+  });
+});
---
+++ youssef-higazy-website/src/app/pages/pages.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PagesRoutingModule } from './pages-routing.module';
+import { HomePageComponent } from './home-page/home-page.component';
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent
+  ],
+  imports: [
+    CommonModule,
+    PagesRoutingModule
+  ],
+  exports: [
+    HomePageComponent
+  ]
+})
+export class PagesModule { }
